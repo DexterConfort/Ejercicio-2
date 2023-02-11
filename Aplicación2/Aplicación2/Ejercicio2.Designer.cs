a50e33efc081c765eb2d2fa64649a2f721e7e8eb
DexterConfort <123494321+DexterConfort@users.noreply.github.com>
2023-02-11 00:49:42 -0600

    Aplicación Terminada
    Commit que carga todo el código de la Aplicación

---
+++ Aplicación2/Aplicación2/Ejercicio2.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Aplicación2
+{
+    partial class Ejercicio2
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Nota1TextBox = new System.Windows.Forms.TextBox();
+            this.Nota2TextBox = new System.Windows.Forms.TextBox();
+            this.Nota3TextBox = new System.Windows.Forms.TextBox();
+            this.Nota4TextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.PromedioTextBox = new System.Windows.Forms.TextBox();
+            this.PosibleErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PosibleErrorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOTAS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "I Parcial:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(37, 130);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "II Parcial:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(37, 178);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "III Parcial:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(37, 219);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "IV Parcial:";
+            // 
+            // Nota1TextBox
+            // 
+            this.Nota1TextBox.Location = new System.Drawing.Point(137, 83);
+            this.Nota1TextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Nota1TextBox.Name = "Nota1TextBox";
+            this.Nota1TextBox.Size = new System.Drawing.Size(91, 23);
+            this.Nota1TextBox.TabIndex = 5;
+            // 
+            // Nota2TextBox
+            // 
+            this.Nota2TextBox.Location = new System.Drawing.Point(137, 130);
+            this.Nota2TextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Nota2TextBox.Name = "Nota2TextBox";
+            this.Nota2TextBox.Size = new System.Drawing.Size(91, 23);
+            this.Nota2TextBox.TabIndex = 6;
+            // 
+            // Nota3TextBox
+            // 
+            this.Nota3TextBox.Location = new System.Drawing.Point(137, 174);
+            this.Nota3TextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Nota3TextBox.Name = "Nota3TextBox";
+            this.Nota3TextBox.Size = new System.Drawing.Size(91, 23);
+            this.Nota3TextBox.TabIndex = 7;
+            // 
+            // Nota4TextBox
+            // 
+            this.Nota4TextBox.Location = new System.Drawing.Point(137, 215);
+            this.Nota4TextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Nota4TextBox.Name = "Nota4TextBox";
+            this.Nota4TextBox.Size = new System.Drawing.Size(91, 23);
+            this.Nota4TextBox.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(412, 43);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 49);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "SOLICITAR PROMEDIO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(313, 109);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Promedio: ";
+            // 
+            // PromedioTextBox
+            // 
+            this.PromedioTextBox.Location = new System.Drawing.Point(412, 109);
+            this.PromedioTextBox.Name = "PromedioTextBox";
+            this.PromedioTextBox.Size = new System.Drawing.Size(120, 23);
+            this.PromedioTextBox.TabIndex = 11;
+            // 
+            // PosibleErrorProvider
+            // 
+            this.PosibleErrorProvider.ContainerControl = this;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(355, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Limpiar Pantalla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Ejercicio2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 301);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.PromedioTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Nota4TextBox);
+            this.Controls.Add(this.Nota3TextBox);
+            this.Controls.Add(this.Nota2TextBox);
+            this.Controls.Add(this.Nota1TextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Ejercicio2";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PosibleErrorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Nota1TextBox;
+        private System.Windows.Forms.TextBox Nota2TextBox;
+        private System.Windows.Forms.TextBox Nota3TextBox;
+        private System.Windows.Forms.TextBox Nota4TextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox PromedioTextBox;
+        private System.Windows.Forms.ErrorProvider PosibleErrorProvider;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Aplicación2/Aplicación2/Ejercicio2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplicación2
+{
+    public partial class Ejercicio2 : Form
+    {
+        public Ejercicio2()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (Nota1TextBox.Text == "")
+            {
+                PosibleErrorProvider.SetError(Nota1TextBox, "Campo vacío");
+                return;
+            }
+            PosibleErrorProvider.Clear();
+            if (Nota2TextBox.Text == "")
+            {
+                PosibleErrorProvider.SetError(Nota2TextBox, "Campo vacío");
+                return;
+            }
+            PosibleErrorProvider.Clear();
+            if (Nota3TextBox.Text == "")
+            {
+                PosibleErrorProvider.SetError(Nota3TextBox, "Campo vacío");
+                return;
+            }
+            if (Nota4TextBox.Text == "")
+            {
+                PosibleErrorProvider.SetError(Nota4TextBox, "Campo vacío");
+                return;
+            }
+            PosibleErrorProvider.Clear();
+
+            decimal nota1 = Convert.ToDecimal(Nota1TextBox.Text);
+            decimal nota2 = Convert.ToDecimal(Nota2TextBox.Text);
+            decimal nota3 = Convert.ToDecimal(Nota3TextBox.Text);
+            decimal nota4 = Convert.ToDecimal(Nota4TextBox.Text);
+
+            decimal promedio = await PromedioAsync(nota1, nota2, nota3, nota4);
+
+            PromedioTextBox.Text = Convert.ToString(promedio);
+        }
+
+        private async Task<decimal> PromedioAsync(decimal nota1, decimal nota2, decimal nota3, decimal nota4)
+        {
+            decimal promedio = await Task.Run(() => {
+                return (nota1 + nota2 + nota3 + nota4) / 4;
+            });
+            return promedio;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PosibleErrorProvider.Clear();
+            Nota1TextBox.Text = "";
+            Nota2TextBox.Text = "";
+            Nota3TextBox.Text = "";
+            Nota4TextBox.Text = "";
+            PromedioTextBox.Text = "";
+        }
+    }
+}
